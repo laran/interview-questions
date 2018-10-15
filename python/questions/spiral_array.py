11d33094e8922185a0351f0cc1f6556e90f0fc4f
Laran Evans <laran@laranevans.com>
2018-10-15 16:19:33 -0700

    Provided sample and moved spiralize to the top of the file to make it easier to read at a glance.

--- python/questions/spiral_array.py
+++ python/questions/spiral_array.py
@@ -1,3 +1,45 @@
+"""
+Usage:
+    spiralize(n)
+
+Example:
+    This code:
+
+        print_spiral(spiralize(3))
+
+    Produces this output:
+
+        1, 2, 3
+        8, 9, 4
+        7, 6, 5
+"""
+
+
+def spiralize(n):
+    """
+    Create a 2-dimensional list of ints with numbers increasing in a
+    clockwise spiral starting from the top left corner ([0][0])
+
+    :param n:
+    :return:
+    """
+    if n == 0:
+        return []
+
+    # fully initialize the matrix
+    a = [] * n
+    for i in range(n):
+        a.append([0] * n)
+
+    for i in range(int(n / 2)):
+        fill(i * n + 1, a, i, n - i - 1, i, n - i - 1)
+
+    if n % 2 == 1:
+        a[int((n - 1) / 2)][int((n - 1) / 2)] = int(n * n)
+
+    return a
+
+
 def fill(i, a, xmin, xmax, ymin, ymax):
     """
     Walk all the way around the matrix at a given depth
@@ -25,31 +67,6 @@ def fill(i, a, xmin, xmax, ymin, ymax):
     for y in range(ymax, 0, -1):
         a[y][xmin] = i
         i += 1
-
-
-def spiralize(n):
-    """
-    Create a 2-dimensional list of ints with numbers increasing in a
-    clockwise spiral starting from the top left corner ([0][0])
-
-    :param n:
-    :return:
-    """
-    if n == 0:
-        return []
-
-    # fully initialize the matrix
-    a = [] * n
-    for i in range(n):
-        a.append([0] * n)
-
-    for i in range(int(n / 2)):
-        fill(i * n + 1, a, i, n - i - 1, i, n - i - 1)
-
-    if n % 2 == 1:
-        a[int((n - 1) / 2)][int((n - 1) / 2)] = int(n * n)
-
-    return a
 
 
 def print_spiral(a2d):
