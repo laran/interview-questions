d47cfb5fdddaca31bfdbacb359be00fe9aba6e8a
Laran Evans <laran@laranevans.com>
2018-10-30 17:20:25 -0700

    Formatting

--- js/tests/one-edit-away.spec.js
+++ js/tests/one-edit-away.spec.js
@@ -2,7 +2,6 @@ import OneEditAway from "../src/one-edit-away";
 
 describe("One Edit Away", () => {
 	it("should pass all sample tests", () => {
-
 		expect(OneEditAway.appliesTo("cat", "dog")).toBe(false);
 		expect(OneEditAway.appliesTo("cat", "cats")).toBe(true);
 		expect(OneEditAway.appliesTo("cat", "cut")).toBe(true);
